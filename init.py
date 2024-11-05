900fb59e36464028d2063d944f1a5141f72c7e64
mromm <mromm@ROMMEL>
2024-11-05 13:42:08 +0100

    add timeoutException import

--- init.py
+++ init.py
@@ -4,6 +4,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 from concurrent.futures import ThreadPoolExecutor
 
 # TensorFlow-Logstufe auf ERROR setzen
